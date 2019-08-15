94d0492ff69eabc6939bf3afd420761c8c9f3a06
Justas Dambrauskas <justas.dambrauskas@vinted.com>
2019-08-15 22:14:47 +0300

    Update kaggle setup helper

--- jstai/colab/kaggle.py
+++ jstai/colab/kaggle.py
@@ -1,6 +1,8 @@
 import os
 
 def copy_kaggle_json():
+  x = os.system("ls kaggle.json")
+  assert x == ['kaggle.json'], 'Upload kaggle.json'
   os.system("mkdir -p ~/.kaggle")
   os.system("cp kaggle.json ~/.kaggle/")
   os.system("chmod 600 ~/.kaggle/kaggle.json")
