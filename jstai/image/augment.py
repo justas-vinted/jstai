7908287932015455321da7f2694a2cbb602a3f8a
Justas Dambrauskas <justas.dambrauskas@vinted.com>
2019-08-19 18:46:48 +0300

    Add image.augment

---
+++ jstai/image/augment.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+
+def augment(image, image_dims=(299, 299), level=0, flips=None, random_crop=False):
+    if random_crop:
+        crop_fraction = 1 - 0.05 * level
+        scaled_dim = tf.to_int32(
+            crop_fraction * tf.to_float(tf.reduce_min(tf.shape(image)[:2]))
+        )
+        image = tf.image.random_crop(image, (scaled_dim, scaled_dim, 3))
+        image = tf.image.resize(image, image_dims)
+
+    if flips in ["horizontal", "both"]:
+        image = tf.image.random_flip_left_right(image)
+    if flips in ["vertical", "both"]:
+        image = tf.image.random_flip_up_down(image)
+
+    if level > 0:
+        lower = 1 - 0.1 * level
+        upper = 1 + 0.1 * level
+        image = tf.image.random_contrast(image, lower=lower, upper=upper)
+        image = tf.image.random_saturation(image, lower=lower, upper=upper)
+        image = tf.clip_by_value(image, 0.0, 1.0)
+    return image
