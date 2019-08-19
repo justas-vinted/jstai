eb192ab9be2e9c6244d661a15cb8a32182bc5fed
Justas Dambrauskas <justas.dambrauskas@vinted.com>
2019-08-19 19:27:42 +0300

    Add plot helpers

--- jstai/plot/__init__.py
+++ jstai/plot/__init__.py
@@ -1,8 +1,1 @@
-import matplotlib.pyplot as plt
-
-def plot_images_dataset(dataset, rows, cols):
-    _, ax = plt.subplots(rows, cols, figsize=(5 * cols, 5 * rows))
-    for i, (x, y) in enumerate(dataset.take(rows * cols)):
-        ax[i // cols, i % cols].axis('off')
-        ax[i // cols, i % cols].imshow(x[0])
-        ax[i // cols, i % cols].set_title(y[0].numpy())
+from .plot_images_dataset import plot_images_dataset
